87d0572dd9a7293d46b06f78afbfa5e9803cc397
Grace Waceke <gracewacekemwangi@gmail.com>
2022-02-26 23:38:10 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,48 @@
+fun main(){
+var text= "akirachix"
+    var char1 =text[0]
+    var char3 =text[2]
+    var char4= text[3]
+    var result = "$char1$char3$char4"
+    println(result)
+    var rope = friend("Grace", 24)
+    println(rope)
+    var String ="Waceke Mwangi"
+    println("the length of my name is " +String.length)
+
+    var name = "Gee"
+    if   (name =="Sam")
+    println("That is me")
+    else{
+        println("That is not me")
+    }
+}
+fun friend(name:String, age:Int): String{
+    var ally = ("Hi, my name is $name and I am $age years old.")
+    return ally
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
